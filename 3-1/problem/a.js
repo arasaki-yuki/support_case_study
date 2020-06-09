6c41206d33f2d892aaa5063b3a658c3c0d77ff91
arasaki-yuki-test <arasaki-yuki@gmo-am.jp>
2020-06-09 17:19:21 +0900

    3-1 ABグループの問題

---
+++ 3-1/problem/a.js
@@ -0,0 +1,38 @@
+const ad = document.querySelector("#ad");
+const max = 10;
+const random = function() {
+  return Math.floor(Math.random() * max + 1);
+};
+
+console.log(random());
+console.log(random());
+console.log(random());
+
+let flg1 = random() === 1;
+let flg2 = random() === 2;
+let flg3 = random() === 3;
+
+const someFunc = function(el) {
+  const creative = `
+    <div
+      style="
+        width: 300px;
+        height: 250px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: salmon;
+        color: #fff;
+        font-size: 24px;
+        text-align: center;
+        margin: 0 auto;
+      "
+    >＼ Ta-da! ／<br>{AD}</div>
+  `;
+  ad.insertAdjacentHTML("beforeend", creative);
+};
+
+if (flg1 && flg2 && flg3) {
+  someFunc(ad);
+} else {
+}
