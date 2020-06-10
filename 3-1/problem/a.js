cb81fb1715db69a3e9e0b6da0ea1c7b3b9482cc1
arasaki-yuki-test <arasaki-yuki@gmo-am.jp>
2020-06-10 10:14:58 +0900

    3-1 ローカル変数

--- 3-1/problem/a.js
+++ 3-1/problem/a.js
@@ -4,30 +4,32 @@ const random = function() {
   return Math.floor(Math.random() * max + 1);
 };
 
-let flg1 = random() === 1;
-let flg2 = random() === 2;
-let flg3 = random() === 3;
+(function(){
+  let flg1 = random() === 1;
+  let flg2 = random() === 2;
+  let flg3 = random() === 3;
 
-const someFunc = function(el) {
-  const creative = `
-    <div
-      style="
-        width: 300px;
-        height: 250px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: salmon;
-        color: #fff;
-        font-size: 24px;
-        text-align: center;
-        margin: 0 auto;
-      "
-    >＼ Ta-da! ／<br>{AD}</div>
-  `;
-  ad.insertAdjacentHTML("beforeend", creative);
-};
+  const someFunc = function(el) {
+    const creative = `
+      <div
+        style="
+          width: 300px;
+          height: 250px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: salmon;
+          color: #fff;
+          font-size: 24px;
+          text-align: center;
+          margin: 0 auto;
+        "
+      >＼ Ta-da! ／<br>{AD}</div>
+    `;
+    ad.insertAdjacentHTML("beforeend", creative);
+  };
 
-if (flg1 && flg2 && flg3) {
-  someFunc(ad);
-}
+  if (flg1 && flg2 && flg3) {
+    someFunc(ad);
+  }
+})();
