e0d0ff58452cf0e8512f5c084ad6f1e45ddd5be3
arasaki-yuki-test <arasaki-yuki@gmo-am.jp>
2020-06-26 02:06:12 +0900

    3-2 problems

---
+++ 3-2/problem/1/ad.js
@@ -0,0 +1,7 @@
+const ad = document.querySelector('#ad');
+ad.innerHTML = `
+<script src="//cdn.gmossp-sp.jp/ads/loader.js?space_id=63858" charset="utf-8" language="JavaScript"></script>
+<script language='javascript'>
+    gmossp_forsp.ad('63858');
+</script>
+`;
